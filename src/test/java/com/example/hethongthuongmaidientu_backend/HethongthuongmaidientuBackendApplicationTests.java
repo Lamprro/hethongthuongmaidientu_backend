a72a4bc5339531c0227ec392f1d975f5bf07596c
Lamprro <140699726+Lamprro@users.noreply.github.com>
2025-10-25 10:54:22 +0700

    commit 1

---
+++ src/test/java/com/example/hethongthuongmaidientu_backend/HethongthuongmaidientuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hethongthuongmaidientu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HethongthuongmaidientuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
